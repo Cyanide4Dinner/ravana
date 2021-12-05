9e513e67b8680b1d7639649c227d0e93db2c90e7
CyanideForBreakfast <dwivedi.utkarsh01@gmail.com>
2021-12-05 09:33:04 +0530

    Add GitHub workflows
    - Add GitHub Actions for building and testing Rust code.
    - Add conditional running of Rust build/test and docs actions.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,6 @@ async fn main(){
     //api::call_api();
     //api::oauth::oauth_process();
     tui::test_tui();
-    events::lifecycle::init().await.unwrap()
+    events::lifecycle::init().await;
 }   
 
